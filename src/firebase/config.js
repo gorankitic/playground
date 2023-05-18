d522654474a587f29291954e6119f10ca0d361e1
Горан Китић <kiticgoran90@gmail.com>
2023-05-18 18:27:39 +0200

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,7 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB7lTJ-GHHfjnfsZyGrExi7ItSZGGOL43E",
-  authDomain: "playground-b7315.firebaseapp.com",
-  projectId: "playground-b7315",
-  storageBucket: "playground-b7315.appspot.com",
-  messagingSenderId: "861676100518",
-  appId: "1:861676100518:web:c474bb114cb8500b52cd67"
+//   firebase config here
 };
 
 // Initialize Firebase
